b810df9b36e488cc823b86251cc0cc27801d7402
sovus_tartar <vad-nesmejanov@yandex.ru>
2023-04-14 21:27:53 +0300

    lab_03 base done

--- 03_systick/systick.c
+++ 03_systick/systick.c
@@ -1,69 +1,46 @@
 #include <stdint.h>
 #include <stdbool.h>
-
-//---------------
-// RCC Registers
-//---------------
-
-#define REG_RCC_CR     (volatile uint32_t*)(uintptr_t)0x40021000U // Clock Control Register
-#define REG_RCC_CFGR   (volatile uint32_t*)(uintptr_t)0x40021004U // PLL Configuration Register
-#define REG_RCC_AHBENR (volatile uint32_t*)(uintptr_t)0x40021014U // AHB1 Peripheral Clock Enable Register
-#define REG_RCC_CFGR2  (volatile uint32_t*)(uintptr_t)0x4002102CU // Clock configuration register 2
-
-//----------------
-// GPIO Registers
-//----------------
-
-#define GPIOC_MODER (volatile uint32_t*)(uintptr_t)0x48000800U // GPIO port mode register
-#define GPIOC_TYPER (volatile uint32_t*)(uintptr_t)0x48000804U // GPIO port output type register
-#define GPIOC_ODR   (volatile uint32_t*)(uintptr_t)0x48000814U // GPIO port output data register
-
-//-------------------
-// SysTick registers
-//-------------------
-
-#define SYSTICK_CSR   (volatile uint32_t*)(uintptr_t)0xE000E010U // SysTick Control and Status Register
-#define SYSTICK_RVR   (volatile uint32_t*)(uintptr_t)0xE000E014U // SysTick Reload Value Register
-#define SYSTICK_CVR   (volatile uint32_t*)(uintptr_t)0xE000E018U // SysTick Current Value Register
-#define SYSTICK_CALIB (volatile uint32_t*)(uintptr_t)0xE000E01CU // SysTick Calibration Value Register
+#include "./led.h"
+#include "./RCC.h"
+#include "./GPIO.h"
+#include "./systick.h"
 
 //-------------------
 // RCC configuration
 //-------------------
 
-#define CPU_FREQENCY 48000000U // CPU frequency: 48 MHz
 #define ONE_MILLISECOND 48000U
+
+struct led_t led8;
+struct led_t led9;
 
 void board_clocking_init()
 {
     // (1) Clock HSE and wait for oscillations to setup.
-    *REG_RCC_CR = 0x00010000U;
-    while ((*REG_RCC_CR & 0x00020000U) != 0x00020000U);
-
+    REG_RCC_CR_HSEON(REG_RCC_CR);
+    WAIT_FOR(REG_RCC_CR, REG_RCC_HSEON_CHECK);
     // (2) Configure PLL:
     // PREDIV output: HSE/2 = 4 MHz
-    *REG_RCC_CFGR2 |= 1U;
+    REG_RCC_CFGR2_CONF_PLL(REG_RCC_CFGR2, 2U);
 
     // (3) Select PREDIV output as PLL input (4 MHz):
-    *REG_RCC_CFGR |= 0x00010000U;
+    REG_RCC_CFGR_PLL_SET_SRC(REG_RCC_CFGR, HSI_SRC);
 
     // (4) Set PLLMUL to 12:
     // SYSCLK frequency = 48 MHz
-    *REG_RCC_CFGR |= (12U-2U) << 18U;
+    REG_RCC_CFGR_PLLMUL_SET_MULT(REG_RCC_CFGR, 12);
 
     // (5) Enable PLL:
-    *REG_RCC_CR |= 0x01000000U;
-    while ((*REG_RCC_CR & 0x02000000U) != 0x02000000U);
-
+    REG_RCC_CR_PLL_ENABLE(REG_RCC_CR);
+    WAIT_FOR(REG_RCC_CR, REG_RCC_CR_PLL_ENABLE_CHECK);
     // (6) Configure AHB frequency to 48 MHz:
-    *REG_RCC_CFGR |= 0b000U << 4U;
+    REG_RCC_CFGR_SET_AHB(REG_RCC_CFGR, AHB_FREQ_48);
 
     // (7) Select PLL as SYSCLK source:
-    *REG_RCC_CFGR |= 0b10U;
-    while ((*REG_RCC_CFGR & 0xCU) != 0x8U);
-
+    REG_RCC_CFGR_SET_SYSCLK_SRC(REG_RCC_CFGR, PLL_SRC);
+    WAIT_FOR(REG_RCC_CFGR, REG_RCC_CFGR_SET_SYSCLK_SRC_CHECK);
     // (8) Set APB frequency to 48 MHz
-    *REG_RCC_CFGR |= 0b000U << 8U;
+    REG_RCC_CFGR_PCLK_PRESCALER_SET_DIV_1(REG_RCC_CFGR);
 }
 
 #define ONE_SEC_DELAY_TIME 3692308U // 48000000 / 13
@@ -79,15 +56,7 @@ void more_precise_delay_forbidden_by_quantum_mechanics_1000ms()
 void board_gpio_init()
 {
     // (1) Configure PC8 and PC9:
-    *REG_RCC_AHBENR |= (1U << 19U);
-
-    // (2) Configure LED modes:
-    *GPIOC_MODER |= (0b01U << (2U*8U));
-    *GPIOC_MODER |= (0b01U << (2U*9U));
-
-    // (3) Configure LED types:
-    *GPIOC_TYPER |= (0U << 8U);
-    *GPIOC_TYPER |= (0U << 9U);
+    REG_RCC_AHBENR_PORT_C_ENABLE(REG_RCC_AHBENR);
 }
 
 //-----------------------
@@ -100,31 +69,42 @@ void systick_init(uint32_t period_us)
     // Assumptions:
     // - There is a reference clock and it can be chosen as clock source.
     // - The SYST_CALIB SKEW bit is 1.
-    uint32_t reload_value = period_us * (CPU_FREQENCY / 1000000U) / 8;
-
     // (1) Program the reload value:
-    *SYSTICK_RVR = (reload_value - 1U) & 0x00FFFFFFU;
+    SYSTICK_RVR_RELOAD_TIME_MS(SYSTICK_RVR, period_us);
 
     // (2) Clear the current value:
-    *SYSTICK_CVR = 0U;
+    SYSTICK_CVR_CLEAR_VAL(SYSTICK_CVR);
 
     // (3) Program the CSR:
     // Watch out for the clock source!
-    *SYSTICK_CSR = 0x3U;
+    SYSTICK_CSR_COUNTER_ENABLE(SYSTICK_CSR);
+    SYSTICK_CSR_TICKINT_ENABLE(SYSTICK_CSR);
+
+    SYSTICK_CALIB_SKEW_EXACT(SYSTICK_CALIB);
 }
 
 void systick_handler(void)
 {
-    static int handler_ticks = 0U;
+    static unsigned handler_ticks = 0U;
+    static unsigned tick_cntr = 0u;
+
 
     handler_ticks += 1U;
 
-    if (handler_ticks == 30000U)
+    if (handler_ticks == 10000U)
     {
         handler_ticks = 0U;
+        tick_cntr  = (tick_cntr + 1) % 2;
 
-        uint32_t reg_gpio_odr = *GPIOC_ODR;
-        *GPIOC_ODR = (reg_gpio_odr & ~0x0100U) | (~reg_gpio_odr & 0x0100U);
+        switch(tick_cntr)
+        {
+            case 0u:
+                led_on(&led9);
+                break;
+            case 1u:
+                led_off(&led9);
+                break;
+        }
     }
 }
 
@@ -134,21 +114,24 @@ void systick_handler(void)
 
 int main(void)
 {
-    board_clocking_init();
 
+    board_clocking_init();
     board_gpio_init();
 
     systick_init(100U);
-/*
+
+    led_init(&led8, GPIOC_BSRR, 8, GPIOC_MODER, GPIOC_TYPER);
+    led_init(&led9, GPIOC_BSRR, 9, GPIOC_MODER, GPIOC_TYPER);
+
     while (1)
     {
         more_precise_delay_forbidden_by_quantum_mechanics_1000ms();
 
-        *GPIOC_ODR |= 0x0200U;
+        led_on(&led8);
 
         more_precise_delay_forbidden_by_quantum_mechanics_1000ms();
 
-        *GPIOC_ODR &= ~0x0200U;
+        led_off(&led8);       
     }
-*/
+
 }
